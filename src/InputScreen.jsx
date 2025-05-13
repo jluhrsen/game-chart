cdcbafe09a966214e7d33078545c538e9d514e25
Jamo Luhrsen <jluhrsen@gmail.com>
2025-05-12 21:14:42 -0700

    add scaffolding for input and report screens
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

---
+++ src/InputScreen.jsx
@@ -0,0 +1,8 @@
+export default function InputScreen() {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Input Screen</h1>
+            {/* form fields go here later */}
+        </div>
+    );
+}
---
+++ src/ReportScreen.jsx
@@ -0,0 +1,8 @@
+export default function ReportScreen() {
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Report Screen</h1>
+            {/* table or stats go here later */}
+        </div>
+    );
+}
